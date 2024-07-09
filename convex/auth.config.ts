08362dc6cbce456984d902e6b1a3fda43379b3be
rakesh <rakeshmanna762@gmail,com>
2024-07-09 12:25:02 +0530

    authentication with clerk done

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+export const authConfig= {
+    providers:[
+        {
+            domain:"https://primary-fox-27.clerk.accounts.dev",
+            applicationID:"convex",
+        },
+    ],
+};
+
+export default authConfig;
